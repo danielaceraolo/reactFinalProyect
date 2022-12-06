5ebb6e8bb167d0096094294720885ddb97a54985
Daniela Ceraolo <105557550+danielaceraolo@users.noreply.github.com>
2022-12-06 16:39:35 -0300

    Delete src/components directory

--- src/components/ItemList/ItemList.jsx
+++
@@ -1,9 +0,0 @@
-import Item from "../Item/Item";
-
-import "./ItemList.css";
-
-const ItemList = ({products}) => {
-        return products.map((prod) => <Item key={prod.id} prod={prod}/>);
-};
-
-export default ItemList;
